c3d9b57b72b4fac7259f626caabc18848f44f9c0
keval511 <kevalprajapati11111@gmail.com>
2024-03-11 23:06:34 +0530

    C

---
+++ 2darray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[4][4]={{8,1,3,2},{5,4,7,6}};
+    
+    cout<<a[0][1]<<'\n';
+    cout<<a[0][3]<<'\n';
+    cout<<a[0][2]<<'\n';
+    cout<<a[1][1]<<'\n';
+    cout<<a[1][0]<<'\n';
+    cout<<a[1][3]<<'\n';
+    cout<<a[1][2]<<'\n';
+    cout<<a[0][0]<<'\n';
+}
---
+++ again swictcase.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter the number of a";
+    cin>>a;
+    cout<<"enter the number of b";
+    cin>>b;
+
+    cout<<"1 for addition";
+    cout<<"2 for subtraction";
+    cout<<"3 for multiplication";
+    cout<<"4 for didvision ";
+
+    cin>>c;
+
+    switch(c)
+    {
+        case 1:
+        cout<<"answer is = "<<a+b;
+        break;
+        case 2:
+        cout<<"answe is = "<<a-b;
+        break;
+        case 3:
+        cout<<"answer is = "<<a*b;
+        break;
+        case 4:
+        cout<<"answer is = a/b";
+        break;
+
+        default:
+        cout<<"incoreect num";
+        
+    }
+}
